fe0bfd2a0463c92ebf21d005c6794f2aa86380f6
sky-colab <55983107+sky-colab@users.noreply.github.com>
2020-06-06 15:30:48 +0100

    first commit

---
+++ precache-manifest.4ab51c867bbbb6f45b972ea46ed63e66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab17fcc55342ae56b36a262ee54b364f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7de2b0a9c2d8432bed75",
+    "url": "./static/css/6.44b7073e.chunk.css"
+  },
+  {
+    "revision": "d4b7b9d60befc18dcb51",
+    "url": "./static/css/main.c12bfc9c.chunk.css"
+  },
+  {
+    "revision": "3afacbd67b6a4c54efe8",
+    "url": "./static/js/0.3ffe27a0.chunk.js"
+  },
+  {
+    "revision": "7de2b0a9c2d8432bed75",
+    "url": "./static/js/6.f83094d2.chunk.js"
+  },
+  {
+    "revision": "ad054f5bdd12847db63b",
+    "url": "./static/js/7.77b24d6d.chunk.js"
+  },
+  {
+    "revision": "439fa87c57630c3060c1",
+    "url": "./static/js/8.6318f375.chunk.js"
+  },
+  {
+    "revision": "714735e906610664b162",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-context-component.54e0d091.chunk.js"
+  },
+  {
+    "revision": "d5b22cb52e8815c2956d",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-group-component.e2bf68ac.chunk.js"
+  },
+  {
+    "revision": "b14d122841b3c41a1d56",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-item.bfc8996b.chunk.js"
+  },
+  {
+    "revision": "d4b7b9d60befc18dcb51",
+    "url": "./static/js/main.58c5340d.chunk.js"
+  },
+  {
+    "revision": "378adfd703eaee90f642",
+    "url": "./static/js/runtime-main.c103d2ca.js"
+  },
+  {
+    "revision": "4ad93c3868d4dc8378646d820f01d1d3",
+    "url": "./static/media/google.4ad93c38.svg"
+  }
+]);
