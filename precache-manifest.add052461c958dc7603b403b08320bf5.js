dafe9706c56b3a431747b483053b42f9e177b80b
Eugy <eugygale@gmail.com>
2020-11-07 21:34:28 +0000

    deploy new ver

---
+++ precache-manifest.add052461c958dc7603b403b08320bf5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc6a205efb355774cd12e1319ede62e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd7b32e6fbf463775bf2",
+    "url": "./static/css/6.04479ec1.chunk.css"
+  },
+  {
+    "revision": "15f7db8c17be4c49f599",
+    "url": "./static/css/main.33068f99.chunk.css"
+  },
+  {
+    "revision": "563bff305ae7033a0d00",
+    "url": "./static/js/0.ebcac1a2.chunk.js"
+  },
+  {
+    "revision": "dd7b32e6fbf463775bf2",
+    "url": "./static/js/6.e3fdaf7e.chunk.js"
+  },
+  {
+    "revision": "6582ebf5a640641e21e0",
+    "url": "./static/js/7.102e88ea.chunk.js"
+  },
+  {
+    "revision": "c050fabdd0adb6beba38",
+    "url": "./static/js/8.6bd0d88e.chunk.js"
+  },
+  {
+    "revision": "652ed679402549ea7f0b",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-context-component.9e4807ab.chunk.js"
+  },
+  {
+    "revision": "c7907f39b27f24ac41ac",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-group-component.ddf52778.chunk.js"
+  },
+  {
+    "revision": "323eab9dade41ee42b2d",
+    "url": "./static/js/@atlaskit/navigation-next/async-chunk/sortable-item.e2795b0b.chunk.js"
+  },
+  {
+    "revision": "15f7db8c17be4c49f599",
+    "url": "./static/js/main.48f368fa.chunk.js"
+  },
+  {
+    "revision": "0b10d8d80637eda4ae46",
+    "url": "./static/js/runtime-main.9015b79d.js"
+  },
+  {
+    "revision": "a70e1046bc5f048015c0e330fb28a6e9",
+    "url": "./static/media/google.a70e1046.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b27d6701335ad46b033fc0823f9d9a5e.js"
+  "./precache-manifest.add052461c958dc7603b403b08320bf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
